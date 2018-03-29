a06ef66860b2bf814be10593f6acc17d04b1c8ea
Oscar Villalta <bagglerock@gmail.com>
2018-03-29 11:27:09 -0400

    made the skeleton for the index.html

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -7,24 +7,13 @@
     storageBucket: "rps-multiplayer-f09f3.appspot.com",
     messagingSenderId: "1014005356723"
   };
+
+  
   firebase.initializeApp(config);
   
   //  Make a variable for the firebase database
   var database = firebase.database();
+
+  //variable for the auth portion of firebase
+  const auth = firebase.auth();
   
-
-  //  Click event listener for submitting a player name
-  $("#click-button").on("click", function() {
-      //  Prevent the page from reloading
-      event.preventDefault();
-      //  Get the input from the name-input field, trim the whitespace
-      var nameInput = $("#name-input");
-      var name = nameInput.val().trim();
-      //  Set name in the database to this name
-      database.ref().set({
-          name: name
-      })
-      //  Clear the form
-      nameInput.val("");
-
-  })
